6b2cbb03659710326385dfe90bf1047d413569fe
unknown <ra99829@uem.br>
2019-11-25 23:11:16 -0300

    Correção nos sucessores necessária

--- Trabalho1/moa.cpp
+++ Trabalho1/moa.cpp
@@ -16,10 +16,10 @@ int main(){
     }*/
     int tab_inicial[4][4] = {{5, 1, 2, 3}, {9,6,7,4}, {13,10,11,8}, {0,14,15,12}};
     node *inicio = new node(tab_inicial, 0, 0, 0, 0, nullptr, pos0[0], pos0[1]);
-    test_constructor(inicio);
+    test_constructor(*inicio);
     int tab_final[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
-    test_constructor(final);
     node *final = new node(tab_final, 0, 0, 0, 16, 0, 4, 4);
+    test_constructor(*final);
     vector<node> sucessores;
     //geraSucessores(inicio, &sucessores);
     //print_sucessores(sucessores);
