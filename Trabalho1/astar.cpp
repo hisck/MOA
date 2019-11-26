96b2b0d93e3b41717fb1ec1534551700428f7d7b
unknown <ra99829@uem.br>
2019-11-25 23:38:47 -0300

    Correção nos sucessores necessária

--- Trabalho1/astar.cpp
+++ Trabalho1/astar.cpp
@@ -172,7 +172,7 @@ int calculate_heuristica(int table[4][4], int choice){
 }
 
 void print_sucessores(vector <node> sucessores){
-    for(int i = 0; i < sucessores.size(); i++){
+    for(int i = 0; i != sucessores.end(); i++){
         for(int j = 0; j < 4; j++){
             printf("|");
             for(int k = 0; k < 4; k++){
