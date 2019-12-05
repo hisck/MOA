fcc3ca167162470172483c4cbb95d7483687813b
unknown <ra99829@uem.br>
2019-12-04 21:14:57 -0300

    aa

--- Trabalho1/moa.cpp
+++ Trabalho1/moa.cpp
@@ -17,10 +17,10 @@ int main(){
     }*/
     //2 3 0 8 1 5 4 7 9  6 10 12 13 14 11  15
     int tab_inicial[4][4] = {{2, 3, 0, 8}, {1,5,4,7}, {9,6,10,12}, {13,14,11,15}};
-    int posXY[2] = {0, 2};
-    node *inicio = new node(tab_inicial, 0, 0, 0, nullptr, posXY);
-    //test_constructor(*inicio);
     int tab_final[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+    int posXY[2] = {0, 2};
+    node *inicio = new node(tab_inicial, 0, calculate_heuristica1(tab_inicial, tab_final), 0, nullptr, posXY);
+    //test_constructor(*inicio);
     int posXY_final[2] = {3 , 3};
     node *final = new node(tab_final, 0, 0, 0, nullptr, posXY_final);
     //test_constructor(*final);
